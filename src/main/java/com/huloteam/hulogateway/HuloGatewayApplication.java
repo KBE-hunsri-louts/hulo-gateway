e9aa5bcbf73569c5a8e7692a07b4e9dfb504c86f
hunsri <>
2022-03-26 18:46:26 +0100

    initial commit

---
+++ src/main/java/com/huloteam/hulogateway/HuloGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.huloteam.hulogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuloGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuloGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huloteam/hulogateway/HuloGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huloteam.hulogateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuloGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
