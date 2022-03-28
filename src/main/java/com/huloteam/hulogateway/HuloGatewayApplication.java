8db0345b9a9aa1c9948657cf60eb98193f34320d
hunsri <>
2022-03-28 18:39:51 +0200

    added Docker

--- src/main/java/com/huloteam/hulogateway/HuloGatewayApplication.java
+++ src/main/java/com/huloteam/hulogateway/HuloGatewayApplication.java
@@ -2,12 +2,22 @@ package com.huloteam.hulogateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class HuloGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HuloGatewayApplication.class, args);
+    }
+
+    @RestController
+    static final class ExampleController {
+        @GetMapping("/")
+        String hello() {
+            return "(^_^)/\n";
+        }
     }
 
 }
