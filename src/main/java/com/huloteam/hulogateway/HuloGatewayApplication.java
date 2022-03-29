cbb0f03a8b5904b98e8d04c6802af9d7a6baa727
hunsri <>
2022-03-29 02:07:00 +0200

    added gateway to VATCalculator

--- src/main/java/com/huloteam/hulogateway/HuloGatewayApplication.java
+++ src/main/java/com/huloteam/hulogateway/HuloGatewayApplication.java
@@ -2,6 +2,9 @@ package com.huloteam.hulogateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,14 +13,6 @@ public class HuloGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HuloGatewayApplication.class, args);
-    }
-
-    @RestController
-    static final class ExampleController {
-        @GetMapping("/")
-        String hello() {
-            return "(^_^)/\n";
-        }
     }
 
 }
---
+++ src/main/java/com/huloteam/hulogateway/router/calculator/VATCalculatorRouter.java
@@ -0,0 +1,28 @@
+package com.huloteam.hulogateway.router.calculator;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VATCalculatorRouter {
+
+    final static int CALCULATOR_API_PORT = 8080;
+    final static String BASE_URI = "http://localhost:"+CALCULATOR_API_PORT;
+
+    /**
+     * Looks for requests for the VATCalculator and forwards them to the VATCalculator microservice.
+     *
+     * @param builder the builder object that is used to rebuild the uri
+     * @return the route locator holding the rerouted uri
+     */
+    @Bean
+    public RouteLocator VATCalculatorRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("rewrite_route", r -> r.host("localhost:8081")
+                        .filters(f -> f.rewritePath("/calculator/(?<segment>.*)", "/calculator/${segment}"))
+                        .uri(BASE_URI))
+                .build();
+    }
+}
