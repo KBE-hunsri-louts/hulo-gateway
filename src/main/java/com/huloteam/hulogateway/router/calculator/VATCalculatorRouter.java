b0503976178ad26c3dae655a4f23b08b75e46198
hunsri <richard.hunstock@gmx.de>
2022-03-29 22:26:42 +0200

    changed port configuration

--- src/main/java/com/huloteam/hulogateway/router/calculator/VATCalculatorRouter.java
+++ src/main/java/com/huloteam/hulogateway/router/calculator/VATCalculatorRouter.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VATCalculatorRouter {
 
-    final static int CALCULATOR_API_PORT = 8080;
+    final static int CALCULATOR_API_PORT = 8100;
     final static String BASE_URI = "http://host.docker.internal:"+CALCULATOR_API_PORT;
 
     /**
@@ -20,7 +20,7 @@ public class VATCalculatorRouter {
     @Bean
     public RouteLocator VATCalculatorRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("rewrite_route", r -> r.host("localhost:8081")
+                .route("rewrite_route", r -> r.host("localhost:8000")
                         .filters(f -> f.rewritePath("/calculator/(?<segment>.*)", "/calculator/${segment}"))
                         .uri(BASE_URI))
                 .build();
